39c6da0c023b7180b01f4c9973d98d1124a8e539
Drocinera <theopichonneau@orange.fr>
2024-04-25 15:56:19 +0200

    Ajout du header, couleur, navigation, css et barre de recherche

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,36 +5,40 @@ import { Link} from "react-router-dom";
 const Header = () => {
   return (
     <header className="header-containeur" id="Top">
-     <nav className="navbar navbar-expand-lg nav-pills ">
+    <nav className="navbar navbar-expand-lg nav-pills ">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img src="./Logo.png" alt="Bootstrap" title="CV Logo" width="40" height="34"/>
         </Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-        <div className="nav justify-content-end" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-underline text-uppercase">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div className="nav justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-underline text-uppercase">
+            <li className="nav-item">
+              <Link className="nav-link"to="/" aria-current="page">Acceuil</Link>
+            </li>
           <li className="nav-item">
-            <Link className="nav-link"to="/" aria-current="page">Acceuil</Link>
+            <Link className="nav-link" to="/bâtiment" >Bâtiment</Link>
           </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/bâtiment" >Bâtiment</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/alimentation" >Alimentation</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/fabrication" >Fabrication</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/services" >Services</Link>
-        </li>
-      </ul>
+          <li className="nav-item">
+            <Link className="nav-link" to="/alimentation" >Alimentation</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/fabrication" >Fabrication</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/services" >Services</Link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search"/>
+          <button class="searchButton btn btn-outline-success" type="submit">Rechercher</button>
+        </form>
+      </div>
     </div>
-  </div>
-</nav>
-    </header>
+  </nav>
+  </header>
   );
 };
 
