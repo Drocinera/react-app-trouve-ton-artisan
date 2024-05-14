dd8caede7d3399a7b8a519f45d13c743a3aeda9a
Drocinera <theopichonneau@orange.fr>
2024-05-14 14:51:14 +0200

    Ajout image, phrase, bouton de retour

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { Link} from "react-router-dom";
     
 const NotFound = () => {
   return (
-  <div>
-    <p>NotFound fonctionne</p>
+  <div className="text-center py-4">
+    <img src="./Image/erreur-404.jpg" alt="Erreur 404" />
+    <h2 className="text-uppercase">Erreur 404</h2>
+    <p>La page demandée n'as pas été trouvée</p>
+    <button className="backButton"><Link to="/" className="linkNotFound text-light">Retourner à la page d'acceuil</Link></button>
   </div>
   );
 };
